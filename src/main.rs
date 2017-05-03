c629103033d70e7937729dba73a8417d89259c73
Julian <hahnjulian@hotmail.de>
2017-05-03 16:52:53 +0200

    Game-change implemented - win needs to be done

--- src/main.rs
+++ src/main.rs
@@ -5,63 +5,60 @@ fn main() {
     println!("Minimal version of 4-wins");
     let mut ground = [[0; 7]; 7];
     let mut player = 1;
-    let mut row: usize;
-    let mut lane: usize;
-    loop{
-    //Build-up
-        p_ground(ground);
-	println!("Player {}, please make your move:", player);
-        println!();
-    //Action-part
-        row = in_row();
-	lane = in_lane();
-        change(&mut ground, row, lane, player);
-	player = if player == 1 {2} else {1};
-    }
+    game(&mut ground, player);
 }
+
+
+fn game(ground: &mut [[i32;7];7], player: i32){
+//whjile win{
+    loop{
+        //Game board
+            p_ground(ground);
+	    println!("Player {}, please make your move:", player);
+        //Input
+	    change(&mut ground, player);
+	    player = if player == 1 {2} else {1};
+	    let win = win(&ground, player);
+        }
+   }
+}
+
 
 //Iterate and print the whole array row by row
 fn p_ground(ground: [[i32; 7]; 7]){
 
-    for x in 0..6 {
+    for x in 0..7 {
         println!("{:?}", ground[x]);
     }
 }
 
-fn in_row()-> usize{
-//    return 2;
+//Input for the lane which is parsed and returned (only existing lanes accepted)
+fn change(ground: &mut[[i32; 7]; 7], player: i32){
+//ground[1][1] = 2;
+//return;
 loop{
-println!("Please input the row:");
-    let mut row = String::new(); //mutable guess and :: associates new of type string
-    io::stdin().read_line(&mut row)
-	.expect("Failed to read line.");
-    let row: usize = match row.trim().parse(){
-	Ok(num) => num,
-	Err(_) => continue,
-    };
-    let row = if row <= 7 {row} else {continue;};
-    return row-1;
-}
-}
-
-fn in_lane()-> usize{
-//    return 2;
-loop{
-println!("Please input the lane:");
+println!("Please provide a lane:");
+//Eingabemechanik
     let mut lane = String::new(); //mutable guess and :: associates new of type string
     io::stdin().read_line(&mut lane)
         .expect("Failed to read line.");
     let lane: usize = match lane.trim().parse(){
 	Ok(num) => num,
-	Err(_) => continue,
+	Err(_) => continue, //If error occures go to the beginning of input
     };
-    let lane = if lane <= 7 {lane} else {continue;};
-    return lane-1;
+//Kontrollmechanik
+    let lane = if lane <= 7 {lane-1} else {println!("You must provide an existing lane!");
+    					continue;}; //If lane is out of bounds go to beginning of input
+    let mut row: usize;
+    row = 6;
+    while ground[row][lane] != 0 && row > 0{
+    row = row -1;
+    }
+    if ground[row][lane] == 0 {
+    ground[row][lane] = player; //If lane is legit set player to board
+    return;} else {
+    println!("Lane is full."); //If lane is full return to beginning of input
+    continue;};
 }
 }
-
-fn change(ground: &mut [[i32; 7]; 7], row: usize, lane: usize, player: i32){
-    ground[row][lane] = player;
-}
-
 
